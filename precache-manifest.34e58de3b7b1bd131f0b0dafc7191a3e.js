54e9b2e7fcc3dfa1454bdd8847e8b208401c26d8
Juan Carlos Campos <rapidclimate@gmail.com>
2019-09-20 15:58:39 -0500

    Add files via upload

---
+++ precache-manifest.34e58de3b7b1bd131f0b0dafc7191a3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/malo2/robots.txt"
+  },
+  {
+    "revision": "e4ae3be260ef8753ca6a",
+    "url": "/malo2/js/chunk-vendors.26c3dc65.js"
+  },
+  {
+    "revision": "06b02b22fcef0cd1ec82",
+    "url": "/malo2/js/app.84e0da68.js"
+  },
+  {
+    "revision": "0a31b1ee2dd9980d0545",
+    "url": "/malo2/js/about.b5fcb637.js"
+  },
+  {
+    "revision": "7045c2c3d29d53aad6a35ea19bc8b4e2",
+    "url": "/malo2/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/malo2/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "06b02b22fcef0cd1ec82",
+    "url": "/malo2/css/app.928b75e0.css"
+  }
+];
